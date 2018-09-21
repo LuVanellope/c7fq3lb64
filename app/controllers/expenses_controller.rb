a1b19ea27d202e07d35ec8eac6f84f766722c864
Lu <luacosta.fc@gmail.com>
2018-09-21 15:02:51 -0500

    adding filter

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,18 @@
 class ExpensesController < ApplicationController
+
   def index
-     @expenses = Expense.order("date DESC")
+    Rails.logger.info(params.inspect)
+    @expenses = Expense.order("date DESC")
+    if params[:concept].present?
+      @expenses = @expenses.where("concept ILIKE ?", "%#{params[:concept]}%")
+    end
+    if params[:category_id].present?
+      @expenses = @expenses.where("category_id = ?", params[:category_id])
+    end
   end
+
+  def search_params
+    params.permit(:category_id, :concept)
+  end
+
 end
